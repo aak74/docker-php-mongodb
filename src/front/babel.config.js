ec6ac8e7d2b3207db90fcb9dc8f273ad3bf9f82c
Andrew Kopylov <andrey.kopylov@arealidea.ru>
2019-04-26 15:52:14 +0300

    attempt for test

--- src/front/babel.config.js
+++ src/front/babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
+    '@babel/preset-env',
     '@vue/app'
-  ]
+  ],
+  ignore: [/[\/\\]core-js/, /@babel[\/\\]runtime/],
+  // "env": {
+  //   "test": {
+  //     "plugins": ["transform-es2015-modules-commonjs"]
+  //   }
+  // }
 }
