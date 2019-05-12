61cd2299453b19e5c0e1acef2b3741b640360942
Andrew Kopylov <riu@inbox.ru>
2019-05-12 10:33:32 +0300

    refinement test

--- src/front/src/services/GetLeftMenu.spec.js
+++ src/front/src/services/GetLeftMenu.spec.js
@@ -1,54 +1,42 @@
 import getLeftMenu from './GetLeftMenu';
 
-const ctx = {};
-// beforeEach(() => {
-ctx.LEFT_MENU = [
-  {
-    title: 'Main',
-    items: [
-      {
-        title: 'dashboard',
-        link: '/',
-      },
-      {
-        title: 'Logout',
-        link: '/logout',
-        description: null,
-      },
-      {
-        title: 'Login',
-        link: '/login',
-        description: null,
-        forUnauthorized: true,
-      },
-    ],
-  },
-];
-// });
+const ctx = {
+  LEFT_MENU: [
+    {
+      title: 'Main',
+      items: [
+        {
+          title: 'dashboard',
+          link: '/',
+        },
+        {
+          title: 'Logout',
+          link: '/logout',
+          description: null,
+        },
+        {
+          title: 'Login',
+          link: '/login',
+          description: null,
+          forUnauthorized: true,
+        },
+      ],
+    },
+  ],
+};
 
-test('Call function without properly params throw error', async () => {
-  expect(getLeftMenu).toThrowError(Error);
+test('Call function without properly params throw error', () => {
+  expect(getLeftMenu).toThrowError(new Error('Bad parameters'));
 });
 
-test('Returns one menu items for an unauthorized user', async () => {
+test('Returns one menu items for an unauthorized user', () => {
   const menu = getLeftMenu(ctx.LEFT_MENU, true);
   expect(menu.length).toBe(1);
   expect(menu[0].items.length).toBe(1);
 });
 
-test('Returns two menu items for an authorized user', async () => {
+test('Returns two menu items for an authorized user', () => {
   const menu = getLeftMenu(ctx.LEFT_MENU, false);
   expect(menu.length).toBe(1);
   expect(menu[0].items.length).toBe(2);
 });
-
-/*
-
-test('Token and RefreshToken are same as set after constructor', async () => {
-  const token = new Token({ token: 'token', refreshToken: 'refreshToken' });
-  token.token = 'newToken';
-  expect(token.token).toBe('newToken');
-  token.refreshToken = 'newRefreshToken';
-  expect(token.refreshToken).toBe('newRefreshToken');
-});
-*/
