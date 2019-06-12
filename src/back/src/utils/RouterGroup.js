cef023a1417193e7615391cd02e23a9a3c1e4fb5
Andrew Kopylov <riu@inbox.ru>
2019-06-12 10:42:12 +0300

    add middlewares

--- src/back/src/utils/RouterGroup.js
+++ src/back/src/utils/RouterGroup.js
@@ -1,22 +1,15 @@
+/* eslint-disable max-len */
+/* eslint-disable no-multi-assign */
 const express = require('express');
 
-// eslint-disable-next-line no-multi-assign
-express.application.group = express.Router.group = function group(arg1, arg2) {
-  let fn;
-  let path;
-
-  if (arg2 === undefined) {
-    path = '/';
-    fn = arg1;
-  } else {
-    path = arg1;
-    fn = arg2;
+express.application.group = express.Router.group = function group(path, routerOrMiddlewares, router) {
+  const expressRouter = express.Router();
+  if (router === undefined) {
+    routerOrMiddlewares(expressRouter);
+    this.use(path, expressRouter);
+    return expressRouter;
   }
-
-  const router = express.Router();
-  // console.log('group', this);
-
-  fn(router);
-  this.use(path, router);
-  return router;
+  router(expressRouter);
+  this.use(path, routerOrMiddlewares, expressRouter);
+  return expressRouter;
 };
